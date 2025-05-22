85dddbea6b9abaa72487c666f4efaf49b9323233
Savannah Lyles <savannahhlyles513@gmail.com>
2025-05-21 22:42:17 -0400

    Set up dashboard route and page

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,23 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
+
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    navigate("/login");
+  };
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard handleLogout={handleLogout} />} />
       </Routes>
     </Router>
   );
